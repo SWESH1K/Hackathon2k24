9e373f4f8901455ec4ae0250c283e5debe226471
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-11-02 21:13:07 +0530

    ChatBot added

--- MoneyHack/home/urls.py
+++ MoneyHack/home/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('expenses/', include("expenses.urls")),
     path('analytics/', include("analytics.urls")),
+    path('chatbot/', include("chatbot.urls")),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
